30c926445ecfcad836510aed14b368eb0b3e268f
Mark <markh4b@hotmail.com>
2018-04-03 21:30:03 +0200

    Scenario: Included round manager from the arena games library
    Added the library as submodule - you will have to create symbolic links
    to the respective folders in your game and/or development directory,
    though.
    
    This adds a simple sound system to the map.

--- ModernCombat.ocd/Libraries.ocd/Scenario.ocd/Script.c
+++ ModernCombat.ocd/Libraries.ocd/Scenario.ocd/Script.c
@@ -5,13 +5,38 @@
 public func Initialize()
 {
 	_inherited(...);
-	
-	InitializeAmbience();
+
+	// Create the round manager - this organizes the game in rounds
+	CreateObject(Environment_RoundManager);
+
+	// Create deco, ...
 	InitializeDeco();
 }
 
 
-/*--- Custom callbacks ---*/
+// Player enters the game
+public func InitializePlayer(int player, int x, int y, object base, int team, id extra_data)
+{
+	// For now, simply block the end of the round.
+	// It may start immediately (later this will not be the case, once we have the main scenario entry)
+	RoundManager()->RegisterRoundEndBlocker(GetHiRank(player));
+	
+	// Do the usual stuff
+	return _inherited(player, x, y, base, team, extra_data, ...);
+}
+
+// Last crew is killed
+public func RelaunchPlayer(int player, int killed_by_player)
+{
+	return _inherited(player, killed_by_player, ...);
+}
+
+
+// Player leaves the game or is eliminated
+public func RemovePlayer(int player, int team)
+{
+	return _inherited(player, team, ...);
+}
 
 
 // Override this function with ambience
@@ -19,11 +44,14 @@ public func Initialize()
 // map, such as sky parallaxity, but
 // do not change over the course of
 // the individual rounds.
+//
+// Note: This is actually an engine callback :)
 private func InitializeAmbience()
 {
 	_inherited(...); // Does nothing for now
 }
 
+/*--- Custom callbacks ---*/
 
 // Override this function to create
 // static deco objects that do not
@@ -36,4 +64,28 @@ private func InitializeAmbience()
 private func InitializeDeco()
 {
 	_inherited(...); // Does nothing for now
+}
+
+/*--- Callbacks from Environment_RoundManager ---*/
+
+// These callbacks happen in order:
+
+// 1) Round is reset to an initial, neutral state
+private func OnRoundReset(int round_number)
+{
+	_inherited(round_number, ...); // Does nothing for now
+}
+
+
+// 2) Round starts, players should be able to act now
+private func OnRoundStart(int round_number)
+{
+	_inherited(round_number, ...); // Does nothing for now
+}
+
+
+// 3) Round is over, clean up things, next call will be the reset
+private func OnRoundEnd(int round_number)
+{
+	_inherited(round_number, ...); // Does nothing for now
 }
