93a002e010e486302f40b6aca4813f0d7e1f2b39
Mark <markh4b@hotmail.com>
2018-04-10 22:41:49 +0200

    Added a dummy pistol.
    Does not work at all at the moment, but that is intended (we have a
    really good safety mechanism here, you cannot even pick it up :D)

---
+++ ModernCombat.ocd/Items.ocd/Weapons.ocd/Pistol.ocd/Script.c
@@ -0,0 +1,60 @@
+#include Library_Firearm
+
+local Name = "$Name$";
+local Description = "$Description$";
+
+public func GetCarryTransform()
+{
+	return Trans_Mul(Trans_Rotate(90, 1, 0, 0), Trans_Translate(-2500, 800, 0), Trans_Scale(800, 800, 800));
+}
+
+
+public func Initialize()
+{
+	_inherited(...);
+	this.MeshTransformation = Trans_Scale(800, 800, 800);
+
+	// fire mode list
+	ClearFiremodes();
+	AddFiremode(FiremodeStandard());
+}
+
+func Definition(id def)
+{
+	def.PictureTransformation = Trans_Mul(Trans_Rotate(-20, 0, 1, 0), Trans_Rotate(-20, 0, 0, 1), Trans_Rotate(5, 1, 0, 0), Trans_Translate(-1800, 0, -3000));
+}
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Fire modes
+
+
+func FiremodeStandard()
+{
+	return
+	{
+		Prototype = fire_mode_default, 
+		name = 				"$Bullets$",
+		mode = 				WEAPON_FM_Single,
+		icon = 				nil,
+		
+		ammo_id = 			CMC_Ammo_Bullets,
+		ammo_load =         15,
+	
+		delay_reload =		40,
+		delay_recover = 	5,
+	
+		damage = 			11,
+	
+		// TODO
+		projectile_id = 	NormalBullet,
+		projectile_speed = 	250,
+		projectile_range =  450,
+		projectile_spread = Projectile_Deviation(1, 1),
+	
+		projectile_distance = 8,
+		projectile_offset_y = -6,
+	};
+}
+
