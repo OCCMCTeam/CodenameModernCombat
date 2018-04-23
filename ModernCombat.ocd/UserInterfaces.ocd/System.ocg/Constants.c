817781f784b34ff96ad9a325eac3cec1566b9f14
Mark <markh4b@hotmail.com>
2018-04-23 22:08:44 +0200

    HUD: First easy modifications
    Added background color to the inventory slots. Created yet another
    System.ocg, because I figured that it would be easier to look for
    HUD-related stuff there, instead of in the generic one.
    
    We can move it, in case that the location is not as desired, of course.

---
+++ ModernCombat.ocd/UserInterfaces.ocd/System.ocg/Constants.c
@@ -0,0 +1,6 @@
+
+/* Default background for GUI elements: black, 30% transparancy = 77 alpha. */
+static const GUI_CMC_Background_Color_Default = 0x4d000000;
+
+/* Backgroudn for highlighted/selected GUI elements: white, 25% transparency = 64 alpha. */
+static const GUI_CMC_Background_Color_Highlight = 0x40ffffff;
