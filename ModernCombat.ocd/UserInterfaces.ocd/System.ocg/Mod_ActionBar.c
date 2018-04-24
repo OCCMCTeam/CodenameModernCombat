96fb25b170d3408e676bea02e4fa7007374a3fd5
Mark <markh4b@hotmail.com>
2018-04-24 20:28:56 +0200

    HUD: Added background color to the action bar

---
+++ ModernCombat.ocd/UserInterfaces.ocd/System.ocg/Mod_ActionBar.c
@@ -0,0 +1,14 @@
+#appendto GUI_Controller_ActionBar
+
+func AssembleActionBar()
+{
+	// Assemble it as usually
+	var action_bar = inherited(...);
+	
+	// Remove the default background graphics and replace it with a rectangle
+	action_bar.Symbol = nil;
+	action_bar.BackgroundColor = GUI_CMC_Background_Color_Default;
+	
+	// Done
+	return action_bar;
+}
