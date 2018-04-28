5eeb4d1d9bdc7a1ab8be4c72a58f5978082e3b0b
Mark <markh4b@hotmail.com>
2018-04-28 20:34:55 +0200

    HUD: Improve visibility on black background
    Changed the colors a bit because black on black does not highlight the
    HUD very well.

--- ModernCombat.ocd/UserInterfaces.ocd/System.ocg/Constants.c
+++ ModernCombat.ocd/UserInterfaces.ocd/System.ocg/Constants.c
@@ -48,8 +48,8 @@ static const GUI_CMC_Element_Large_Height = 60;
 
 /* --- Colors --- */
 
-/* Default background for GUI elements: black, 30% transparancy = 77 alpha. */
-static const GUI_CMC_Background_Color_Default =   0x4d000000;
+/* Default background for GUI elements: dark grey (38, 38, 38), 60% transparancy = 102 alpha. */
+static const GUI_CMC_Background_Color_Default =   0x66262626;
 
 /* Background for highlighted/selected GUI elements: white, 25% transparency = 64 alpha. */
 static const GUI_CMC_Background_Color_Highlight = 0x40ffffff;
