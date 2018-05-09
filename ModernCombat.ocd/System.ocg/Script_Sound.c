eb5be6db253d17cccc3f44bfa8103453ff2cda9d
Mark <markh4b@hotmail.com>
2018-05-09 10:06:24 +0200

    Sound: Support multiple sounds via proplist option

--- ModernCombat.ocd/System.ocg/Script_Sound.c
+++ ModernCombat.ocd/System.ocg/Script_Sound.c
@@ -7,6 +7,7 @@
 
 /*
  Modification to Sound():
+ a) Playing one sound for a specific player and a different sound for other players:
  - You may pass two proplists as parameters: Sound(name, options)
  - The options proplist can be used to set the parameters of the Sound function except "name", see the official docu
  - The options proplist must contain the .player setting; This defines for which player the sound should be exclusive
@@ -14,9 +15,21 @@
  - The property "self" determines, which sound is played for options.player; No sound will be played for this player if the value is nil
  - The property "others" determines, which sound is played for all other players of type C4PT_User; No sound will be played if the value is nil
  - Note that "others" = nil is redundant, you could simply call Sound("foo", {player = some_index})
+ b) Playing one sound before it was faded out:
+ - Pass a proplist with the property "multiple = true".
  */
 global func Sound(name, opts, ...)
 {
+	// Support multiple sounds; FIXME - should be backported to OC
+	if (GetType(opts) == C4V_PropList && opts.multiple)
+	{
+		var source = CreateObject(Dummy, 0, 0, NO_OWNER);
+		source.Visibility = VIS_All;
+		var result = source->Sound(name, { Prototype = opts, multiple = false});
+		source->RemoveObject();
+		return result;
+	}
+
 	if (GetType(name) == C4V_PropList)
 	{
 		// The opts parameter can be a proplist, see OC System.ocg/NamedParams.c
