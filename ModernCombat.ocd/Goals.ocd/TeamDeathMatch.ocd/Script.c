32693840a551048a94dd741e86a17efc3ee222a2
Mark <markh4b@hotmail.com>
2018-05-17 22:32:27 +0200

    Goals: Team deathmatch with timeout
    Very non-functional at the moment, but hey, the timeout is there :I

--- ModernCombat.ocd/Goals.ocd/TeamDeathMatch.ocd/Script.c
+++ ModernCombat.ocd/Goals.ocd/TeamDeathMatch.ocd/Script.c
@@ -1,2 +1,11 @@
 #include Arena_Goal_TeamDeathMatch
+#include Plugin_Goal_TimeLimit
 
+
+func OnRoundStart(int round)
+{
+	inherited(...);
+	
+	var clock = CreateObject(GUI_Clock);
+	SetClock(clock);
+}
---
+++ ModernCombat.ocf/CMC_MacrossFrontier.ocs/System.ocg/Game_Goals.c
@@ -0,0 +1,6 @@
+#appendto CMC_Game_Session_Configurator
+
+func GetAvailableGoals()
+{
+	return [CMC_Goal_TeamDeathMatch];
+}
