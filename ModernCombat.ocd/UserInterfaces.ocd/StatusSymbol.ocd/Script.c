c3d1f98b60ff87b256a6a532b7d694928d0920c4
Mark <markh4b@hotmail.com>
2018-05-27 14:10:29 +0200

    Satus symbol: Muliple symbols mechanics

--- ModernCombat.ocd/UserInterfaces.ocd/StatusSymbol.ocd/Script.c
+++ ModernCombat.ocd/UserInterfaces.ocd/StatusSymbol.ocd/Script.c
@@ -8,7 +8,7 @@
 
 /* --- Properties --- */
 
-local symbol;
+local symbols;
 
 local Name = "$Name$";
 local Description = "$Description$";
@@ -44,35 +44,37 @@ func SaveScenarioObject() { return false; }
 
 public func GetStatusSymbolHelper(object to)
 {
-	var obj = FindObject(Find_ID(StatusSymbol), Find_ActionTarget(to));
-	if (obj) 
-		return obj;
-	obj = CreateObject(StatusSymbol, 0, 0, to->GetOwner());
-	obj->Init(to);
-	return obj;
+	AssertDefinitionContext();	
+	var helper = FindObject(Find_ID(this), Find_ActionTarget(to));
+	if (!helper)
+	{
+		helper = CreateObject(this, 0, 0, to->GetOwner());
+		helper->Init(to);
+	}
+	return helper;
 }
 
 
-global func ShowStatusSymbol(id symbol)
+global func ShowStatusSymbol(id symbol_id, int priority, string graphics_name, int visibility, bool blink)
 {
 	if (this) 
 	{
-		var helper = StatusSymbol->GetStatusSymbolHelper(this);
-		if (helper) 
+		var helper = CMC_StatusSymbol->GetStatusSymbolHelper(this);
+		if (helper)
 		{
-			helper->AddSymbol(symbol);
-			return true;
+			helper->AddSymbol(symbol_id, graphics_name, priority, visibility, blink);
+			return helper;
 		}
 	}
-	return false;
+	return helper;
 }
 
 
 global func RemoveStatusSymbol(id symbol)
 {
-	if (this) 
+	if (this)
 	{
-		var helper = StatusSymbol->GetStatusSymbolHelper(this);
+		var helper = CMC_StatusSymbol->GetStatusSymbolHelper(this);
 		if (helper) 
 		{
 			helper->RemoveSymbol(symbol);
@@ -86,11 +88,13 @@ global func RemoveStatusSymbol(id symbol)
 
 func Init(object to)
 {
+	SetOwner(to->GetOwner());
 	SetAction("Be", to);
+	symbols = {};
 	
 	// Above the object.
 	var height = to->GetDefCoreVal("Height", "DefCore") / 2;
-	SetVertex(0, VTX_Y, height);
+	SetVertex(0, VTX_Y, height, 1);
 	var x = to->GetVertex(0, VTX_X);
 	SetVertex(0, VTX_X, x);
 
@@ -99,67 +103,97 @@ func Init(object to)
 	return;
 }
 
-func AddSymbol(id symbol_id)
+func AddSymbol(id symbol_id, string graphics_name, int priority, int visibility, bool blink)
 {
-	symbol = symbol_id;
+	symbols[Format("%v", symbol_id)] = 
+	{
+		Symbol = symbol_id,
+		GraphicsName = graphics_name,
+		Priority = priority,
+		Added = FrameCounter(),
+		Blink = blink,
+		Visibility = visibility ?? GetActionTarget().Visibility,
+	};
 	Update();
 }
 
-func RemoveSymbol(id symbol_id)
+func RemoveSymbol(id symbol_id, int priority)
 {
-	if (symbol != symbol_id)
-		return;
-	symbol = nil;
+	symbols[Format("%v", symbol_id)] = nil;
 	Update();
+}
+
+func GetSymbol(id symbol_id)
+{
+	return symbols[Format("%v", symbol_id)];
 }
 
 func Update()
 {
+	var symbol;
+	for (var current in GetProperties(symbols))
+	{
+		if (symbols[current] &&
+		   (!symbol
+		    || (symbols[current].Priority > symbol.Priority)
+		    || (symbols[current].Priority == symbol.Priority && symbols[current].Added > symbol.Added)))
+		{
+			symbol = symbols[current];
+			Log("New symbol is %v", symbol);
+		}
+	}
+
 	if (!symbol)
 	{
-		SetGraphics();
+		SetGraphics(nil, nil, 1);
 		this.Visibility = VIS_None;
-		return;
+		Blink(false);
 	}
-	SetShape(symbol->GetDefOffset(0), symbol->GetDefOffset(1), symbol->GetDefWidth(), symbol->GetDefHeight());
-	SetGraphics(nil, symbol);
-	Blink();
-	return;
+	else
+	{
+		SetShape(symbol.Symbol->GetDefOffset(0), symbol.Symbol->GetDefOffset(1), symbol.Symbol->GetDefWidth(), symbol.Symbol->GetDefHeight());
+		SetGraphics(symbol.GraphicsName, symbol.Symbol, 1, GFXOV_MODE_Base);
+		this.Visibility = symbol.Visibility;
+		Blink(symbol.Blink);
+	}
 }
 
 /* --- Optional: Blinking --- */
 
-public func Blink()
+public func Blink(bool should_blink, int visibility)
 {
-	if (GetEffect("Blinking", this))
-		RemoveEffect("Blinking", this);
-	AddEffect("Blinking", this, 1, 16, this);
-	return;
+	RemoveEffect("Blinking", this);
+	if (should_blink)
+	{
+		CreateEffect(Blinking, 1, 16, visibility);
+	}
 }
 
-protected func FxBlinkingStart(object target, proplist effect, int temp)
+local Blinking = new Effect 
 {
-	if (temp) 
+	Start = func (int temp)
+	{
+		if (!temp)
+		{
+			this.Interval = 16;
+			this.Visibility = this.Target.Visibility;
+			this.Visible = true;
+		}
 		return FX_OK;
-	// Set interval to a fixed number of frames.
-	effect.Interval = 16;
-	// Set initial visibility to visible.
-	this.Visibility = this->GetActionTarget().Visibility;
-	effect.visible = true;	
-	return FX_OK;
-}
-
-protected func FxBlinkingTimer(object target, proplist effect)
-{
-	if (effect.visible)
+	},
+	
+	Timer = func ()
 	{
-		effect.visible = false;	
-		this.Visibility = VIS_None;
+		if (this.Visible)
+		{
+			this.Visible = false;
+			this.Target.Visibility = VIS_None;
+		}
+		else
+		{
+			this.Visible = true;
+			this.Target.Visibility = this.Visibility;
+		}
+		return FX_OK;
 	}
-	else
-	{
-		effect.visible = true;	
-		this.Visibility = this->GetActionTarget().Visibility;
-	}
-	return FX_OK;
-}
+};
