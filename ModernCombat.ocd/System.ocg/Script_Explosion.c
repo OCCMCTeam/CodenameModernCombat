b413ce764291644cdb1079104fe3f11e1c85c1cc
Mark <markh4b@hotmail.com>
2018-11-24 17:13:18 +0100

    Issue #5: Fix explosion strength
    Multiple shockwaves pushed shockwaved objects even more. Wanted to
    avoid changing original code, but alas, not it is so.

--- ModernCombat.ocd/System.ocg/Script_Explosion.c
+++ ModernCombat.ocd/System.ocg/Script_Explosion.c
@@ -44,11 +44,84 @@ global func Explosion(array radius_stages, array damage_stages, bool silent, boo
 
 		if (i > 0)
 		{
-			BlastObjects(GetX(), GetY(), radius_stages[i], Contained(), GetController(), damage * multiplier, GetObjectLayer());
+			BlastObjects(GetX(), GetY(), radius_stages[i], Contained(), GetController(), damage * multiplier, GetObjectLayer(), nil, true);
 		}
 		else
 		{
 			Explode(radius_stages[i], silent, damage * multiplier);
 		}
 	}
+}
+
+//------------------------------------------------------------------------------------------------------------
+//
+// Code below is from System.ocg/Explode.c
+//
+// Has some issues, in my opinion, should be modularized again in OC. Will mark the actual changes
+// TODO (as a reminder to remove this one day.)
+
+/*-- Blast objects & shockwaves --*/
+
+// Damage and hurl objects away.
+// documented in /docs/sdk/script/fn
+global func BlastObjects(int x, int y, int level, object container, int cause_plr, int damage_level, object layer, object prev_container, bool no_shockwave)
+{
+	var obj;
+	
+	// Coordinates are always supplied globally, convert to local coordinates.
+	var l_x = x - GetX(), l_y = y - GetY();
+	
+	// caused by: if not specified, controller of calling object
+	if (cause_plr == nil)
+		if (this)
+			cause_plr = GetController();
+	
+	// damage: if not specified this is the same as the explosion radius
+	if (damage_level == nil)
+		damage_level = level;
+	
+	// In a container?
+	if (container)
+	{
+		if (container->GetObjectLayer() == layer)
+		{
+			container->BlastObject(damage_level, cause_plr);
+			if (!container)
+				return true; // Container could be removed in the meanwhile.
+			for (obj in FindObjects(Find_Container(container), Find_Layer(layer), Find_Exclude(prev_container)))
+				if (obj)
+					obj->BlastObject(damage_level, cause_plr);
+		}
+	}
+	else
+	{
+		// Object is outside.
+		var at_rect = Find_AtRect(l_x - 5, l_y - 5, 10, 10);
+		// Damage objects at point of explosion.
+		for (var obj in FindObjects(at_rect, Find_NoContainer(), Find_Layer(layer), Find_Exclude(prev_container)))
+			if (obj) obj->BlastObject(damage_level, cause_plr);
+			
+		// Damage objects in radius.
+		for (var obj in FindObjects(Find_Distance(level, l_x, l_y), Find_Not(at_rect), Find_NoContainer(), Find_Layer(layer), Find_Exclude(prev_container)))
+			if (obj) obj->BlastObject(damage_level / 2, cause_plr);
+
+		
+		// Perform the shockwave at the location where the top level container previously was.
+		// This ensures reliable flint jumps for explosives that explode inside a crew member.
+		var off_x = 0, off_y = 0;
+		if (prev_container)
+		{	
+			var max_offset = 300;
+			off_x = BoundBy(-prev_container->GetXDir(100), -max_offset, max_offset);
+			off_y = BoundBy(-prev_container->GetYDir(100), -max_offset, max_offset);			
+		}
+		// Original code: DoShockwave(x, y, level, cause_plr, layer, off_x, off_y);
+		// Modified:
+		if (!no_shockwave)
+		{
+			DoShockwave(x, y, level, cause_plr, layer, off_x, off_y);
+		}
+	}
+	// Done.
+	return true;
 }
